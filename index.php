cf0784ce32af37f89e7f9444ca135435b72b54d1
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-05-08 16:28:46 +0700

    update

--- index.php
+++ index.php
@@ -2,4 +2,64 @@
 <?php $is_home = true; ?>
 <?php require "template-parts/layouts/header.php"; ?>
 
+<!--Tuyển dụng thuê Headhunter là gì?-->
+<div class="home__lagi uk-section-xsmall">
+    <div class="uk-container">
+        <ul class="uk-breadcrumb home__lagi__breadcrumb">
+            <li><a href="#">Trang chủ</a></li>
+            <li><a href="#">việc làm</a></li>
+            <li><span>headhunter</span></li>
+        </ul>
+        <div class="uk-margin uk-text-center">
+            <h3 class="uk-h3 uk-margin-remove home__visao__title1"><span>Tuyển dụng thuê Headhunter là gì?</span></h3>
+        </div>
+        <article class="uk-article home__lagi__article">
+            <p>Headhunter” là một thuật ngữ gốc tiếng Anh, dịch ra tiếng Việt có nghĩa là thợ săn chất xám.</p>
+            <p>Headhunter là cụm từ chỉ những người làm trong nhóm ngành nhân sự chuyên đi săn chất xám, nhân tài theo các đơn đặt hàng từ các công ty khách hàng hoặc cho chính công ty của mình.</p>
+            <p>Dịch vụ tuyển dụng thuê Headhunter là dịch vụ chuyên dụng mà doanh nghiệp sẽ trả tiền để tuyển dụng nhân viên cho mình,</p>
+        </article>
+    </div>
+</div>
+<!--/Tuyển dụng thuê Headhunter là gì?-->
+
+<!--Vì sao bạn nên lựa chọn dịch vụ tuyển dụng thuê tại vieclam123.vn-->
+<div class="home__visao uk-background-01 uk-section-xsmall">
+    <div class="uk-container">
+        <div class="uk-margin uk-text-center">
+            <h3 class="uk-h3 uk-margin-remove home__visao__title1"><span>Vì sao bạn nên lựa chọn dịch vụ tuyển dụng thuê tại vieclam123.vn</span></h3>
+        </div>
+        <div class="uk-child-width-1-1" uk-grid>
+            <?php
+            $data = array(
+                array(
+                    'src' => '',
+                    'title' => '',
+                    'desc' => '',
+                ),
+                array(
+                    'src' => '',
+                    'title' => '',
+                    'desc' => '',
+                ),
+                array(
+                    'src' => '',
+                    'title' => '',
+                    'desc' => '',
+                ),
+                array(
+                    'src' => '',
+                    'title' => '',
+                    'desc' => '',
+                ),
+            );
+            ?>
+            <div>
+
+            </div>
+            <?php ?>
+        </div>
+    </div>
+</div>
+<!--/Vì sao bạn nên lựa chọn dịch vụ tuyển dụng thuê tại vieclam123.vn-->
+
 <?php require "template-parts/layouts/footer.php"; ?>
